226eb4ebbe3e04dad60d1670fcb3aad3987e9180
Joilson M S Lopes <joilsonmirandasl@hotmail.com>
2021-09-26 20:19:29 -0300

    add favicon and meta tags

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -43,6 +43,20 @@ export default class MyDocument extends Document {
                 <Head>
                     <link rel="preconnect" href="https://fonts.googleapis.com" />
                     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@300;400;700&display=swap" rel="stylesheet" />
+
+                    <meta property="og:title" content="Tabela Fipe" />
+                    <meta property="og:site_name" content="Tabela Fipe" />
+                    <meta property="og:description" content="Consulte o valor de um veículo de forma gratuita." />
+                    <meta property="og:image" content="/placeholder-facebook-meta.jpg" />
+                    <meta property="og:type" content="website" />
+
+                    <meta name="twitter:site" content="Tabela Fipe" />
+                    <meta name="twitter:title" content="Tabela Fipe" />
+                    <meta name="twitter:description" content="Consulte o valor de um veículo de forma gratuita." />
+                    <meta name="twitter:image" content="/placeholder-facebook-meta.jpg" />
+                    <meta name="twitter:card" content="summary" />
+
+                    <link rel="shortcut icon" href="/favicon.png" type="image/png" />
                 </Head>
                 <body>
                     <Main />
